220a589ee6f9e645e06b486958cec6a42485978c
Gabriel-Rosch <gabrielrosch@gmail.com>
2019-12-09 11:49:50 -0300

    config postgress

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,23 @@
 require('dotenv/config');
 
+// module.exports = {
+//   dialect: 'postgres',
+//   host: '192.168.99.100',
+//   username: 'postgres',
+//   password: 'masterkey',
+//   database: 'postgres',
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
-  username: 'postgres',
-  password: 'masterkey',
-  database: 'postgres',
+  host: 'ec2-174-129-255-59.compute-1.amazonaws.com',
+  username: 'lrkbhqmucdwvub',
+  password: 'fd244f649337f7bd97637f15770a533437f1be3b31b8dbea23b907a2e5df50c0',
+  database: 'deatt2karfltq9',
   define: {
     timestamps: true,
     underscored: true,
