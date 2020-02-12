0927a97acf1d7ab37bdef759a7d62805e50829e5
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-02-12 20:21:01 -0300

    Enviar

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,16 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: 'postgres',
-  host: '192.168.99.100',
-  username: 'postgres',
-  password: 'masterkey',
-  database: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'masterkey',
+    database: 'postgres',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
 };
 // module.exports = {
 //   dialect: 'postgres',
