25990d62296a20dfa12b906f1191a37ede809d61
Gabriel-Rosch <gabrielrosch@gmail.com>
2020-03-09 20:22:30 -0300

    mensages

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -16,11 +16,8 @@ class SessionController {
 
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(401).json({ error: 'Usuário não encontrado' });
-    }
-    if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Senha incorreta' });
+    if (!user || password) {
+      return res.status(401).json({ error: 'Usuário ou senha incorreto' });
     }
     const { id, name } = user;
     return res.json({
