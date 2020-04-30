5a540e4af16cfec71e8348e848e4f958e950333c
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-04-29 21:54:11 -0300

    Teste

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ require('dotenv/config');
 
 module.exports = {
     dialect: 'postgres',
-    host: '192.168.99.100',
     username: 'postgres',
     password: 'masterkey',
     database: 'postgres',
